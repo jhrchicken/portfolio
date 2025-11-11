2a2c78c5261ef1aa5286e14d84dbdfcff50d1a7f
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2025-11-11 21:16:56 +0900

    네비게이션바 수정

--- js/index.js
+++ js/index.js
@@ -8,3 +8,51 @@ async function includeHTML(selector, file) {
   const response = await fetch(file);
   element.innerHTML = await response.text();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  window.addEventListener('scroll', () => {
+    let scrollY = window.pageYOffset;
+    let current = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionHeight = section.offsetHeight;
+      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 2) {
+      current = 'contact';
+    }
+
+    if (scrollY < 100) {
+      current = 'home';
+    }
+
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === `#${current}`) {
+        link.classList.add('active');
+      }
+    });
+  });
+
+  document.querySelectorAll('.nav-link').forEach((link) => {
+    link.addEventListener('click', function (e) {
+      const href = this.getAttribute('href');
+
+      if (href === '#home') {
+        e.preventDefault();
+
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }
+    });
+  });
+})
